a90d3505ceb82d94982605b20eac520f225e9c7e
libjared <jared@perrycode.com>
2017-09-06 16:05:44 -0400

    wire up all toolbar buttons

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -2,34 +2,34 @@ import React, { Component } from 'react';
 import './Toolbar.css'
 import MaskButton from './MaskButton.js';
 import ImageShiftRGB from './buttons/shiftrgb.svg';
-import ImageXOR from './buttons/shiftrgb.svg';
+import ImageXOR from './buttons/xor.svg';
 import ImageInvert from './buttons/invert.svg';
-import ImageFlipVert from './buttons/shiftrgb.svg';
-import ImageFlipHoriz from './buttons/shiftrgb.svg';
-import ImageVertGlass from './buttons/shiftrgb.svg';
-import ImageHorizGlass from './buttons/shiftrgb.svg';
-import ImageWin from './buttons/shiftrgb.svg';
+import ImageFlipVert from './buttons/flipvert.svg';
+import ImageFlipHoriz from './buttons/fliphoriz.svg';
+import ImageVertGlass from './buttons/vertglass.svg';
+import ImageHorizGlass from './buttons/horizglass.svg';
+import ImageWin from './buttons/win.svg';
 import ImageMekoPlus from './buttons/mekoplus.svg';
 import ImageMekoMinus from './buttons/mekominus.svg';
 import ImageFL from './buttons/fl.svg';
-import ImageQ0 from './buttons/shiftrgb.svg';
+import ImageQ0 from './buttons/q0.svg';
 
 class Toolbar extends Component {
   render() {
     return (
       <div className="toolbar">
-        <MaskButton text="Shift RGB" img={ImageShiftRGB} />
-        <MaskButton text="XOR 0x80" img={ImageXOR} />
+        <MaskButton text="Shift RGB" img={ImageShiftRGB} onClick={() => this.props.addMask("shiftrgb")} />
+        <MaskButton text="XOR 0x80" img={ImageXOR} onClick={() => this.props.addMask("xor")} />
         <MaskButton text="Invert" img={ImageInvert} onClick={() => this.props.addMask("invert")} />
-        <MaskButton text="Flip Vertically" img={ImageFlipVert} />
-        <MaskButton text="Flip Horizontally" img={ImageFlipHoriz} />
-        <MaskButton text="Vertical Glass" img={ImageVertGlass} />
-        <MaskButton text="Horizontal Glass" img={ImageHorizGlass} />
-        <MaskButton text="Win" img={ImageWin} />
-        <MaskButton text="Meko +" img={ImageMekoPlus} />
-        <MaskButton text="Meko -" img={ImageMekoMinus} />
+        <MaskButton text="Flip Vertically" img={ImageFlipVert} onClick={() => this.props.addMask("flipvert")} />
+        <MaskButton text="Flip Horizontally" img={ImageFlipHoriz} onClick={() => this.props.addMask("fliphoriz")} />
+        <MaskButton text="Vertical Glass" img={ImageVertGlass} onClick={() => this.props.addMask("vertglass")} />
+        <MaskButton text="Horizontal Glass" img={ImageHorizGlass} onClick={() => this.props.addMask("horizglass")} />
+        <MaskButton text="Win" img={ImageWin} onClick={() => this.props.addMask("win")} />
+        <MaskButton text="Meko +" img={ImageMekoPlus} onClick={() => this.props.addMask("mekoplus")} />
+        <MaskButton text="Meko -" img={ImageMekoMinus} onClick={() => this.props.addMask("mekominus")} />
         <MaskButton text="FL" img={ImageFL} onClick={() => this.props.addMask("fl")} />
-        <MaskButton text="Q0" img={ImageQ0} />
+        <MaskButton text="Q0" img={ImageQ0} onClick={() => this.props.addMask("q0")} />
       </div>
     );
   }
