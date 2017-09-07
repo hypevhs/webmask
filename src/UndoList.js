0b7ede152969394fb617da2511cbca6ebd540faf
libjared <jared@perrycode.com>
2017-09-07 17:26:57 -0400

    really bad undo list css

--- src/UndoList.js
+++ src/UndoList.js
@@ -1,34 +1,47 @@
 import React, { Component } from 'react';
-//import './UndoList.css';
+import './UndoList.css';
 
 class UndoList extends Component {
+  constructor(props) {
+    super(props);
+    this.undoToHere = this.undoToHere.bind(this);
+  }
+
   render() {
     return (
-      <table className="undo-list">
-        <thead>
-          <tr>
-            <td>{this.props.masks.length} masks</td>
-          </tr>
-          <tr>
-            <td>Type</td>
-            <td>Area</td>
-          </tr>
-        </thead>
-        <tbody>
-          {this.getRows()}
-        </tbody>
-      </table>
+      <div className="undo-list">
+        <table className="undo-list-table-header">
+          <thead>
+            {/* <tr>
+              <th>{this.props.masks.length} masks</th>
+            </tr> */}
+            <tr>
+              <th>Type</th>
+              <th>Area</th>
+            </tr>
+          </thead>
+        </table>
+        <table className="undo-list-table">
+          <tbody>
+            {this.getRows()}
+          </tbody>
+        </table>
+      </div>
     );
   }
 
   getRows() {
     var rows = this.props.masks.map((here, idx) => {
-      return <tr key={idx}>
+      return <tr key={idx} onClick={this.undoToHere}>
         <td>{here.type}</td>
         <td>{`{${here.x}, ${here.y}, ${here.w}, ${here.h}}`}</td>
       </tr>;
     });
     return rows;
+  }
+
+  undoToHere(e) {
+    console.log(e, e.relatedTarget);
   }
 }
 
