a31fd3a5cb653c49d8868bb644ebc43557694d2e
libjared <jared@perrycode.com>
2017-09-15 11:18:58 -0400

    cleanup flmask

--- src/masks/FLMask.js
+++ src/masks/FLMask.js
@@ -1,3 +1,5 @@
+import { CopyBlock } from './Common.js';
+
 /**
  * @class FLMask
  * @see https://github.com/zqad/zmask/blob/master/src/org/zkt/zmask/masks/FL.java
@@ -6,7 +8,6 @@ class FLMask {
   applyMask(ctx, sel) {
     ctx.save();
 
-    // get parameters
     var width = sel.w;
     var height = sel.h;
     var cellWidth = width / 8;
@@ -16,20 +17,15 @@ class FLMask {
     console.assert(height % 8 === 0);
     console.assert(width % 8 === 0);
 
-    // get FLTable
     var table = this.buildTable(cellWidth, cellHeight);
-
-    // extract into source buffer
     var srcData = ctx.getImageData(sel.x, sel.y, sel.w, sel.h);
+    var destData = ctx.createImageData(srcData); // create empty destination buffer
 
-    // create empty destination buffer
-    var destData = ctx.createImageData(srcData);
-
-    // srcData buffer -> magical rearrangement dictionary -> destData buffer
+    // srcData buffer -> magical rearrangement table -> destData buffer
     for (var y = 0; y < cellHeight; y++) {
       for (var x = 0; x < cellWidth; x++) {
         var tr = this.tableTransform(table, x, y);
-        this.transferCell(srcData, destData, x*8, y*8, tr.x*8, tr.y*8, tr.inv);
+        CopyBlock(srcData, destData, x*8, y*8, tr.x*8, tr.y*8, tr.inv);
       }
     }
 
@@ -40,41 +36,14 @@ class FLMask {
   }
 
   /**
-   * transfer an 8x8 cell starting from x,y from one buffer to another
-   * @param {ImageData} src image buffer we copy from
-   * @param {ImageData} dest image buffer we copy to
-   * @param {number} srcX pixel coordinate X of source cell's topleftmost pixel
-   * @param {number} srcY pixel coordinate Y of destination cell's topleftmost pixel
-   * @param {number} destX pixel coordinate X of source cell's topleftmost pixel
-   * @param {number} destY pixel coordinate Y of destination cell's topleftmost pixel
-   * @param {boolean} invert whether to invert the RGB of this cell
+   * Build an FL transformation table that can operate on images of a given width and height.
+   * This is mostly magic math stuff.
+   *
+   * @param {number} width
+   * @param {number} height
+   * @returns
+   * @memberof FLMask
    */
-  transferCell(src, dest, srcX, srcY, destX, destY, invert) {
-    for (var y = 0; y < 8; y++) {
-      for (var x = 0; x < 8; x++) {
-        var srcRealX  = srcX  + x;
-        var srcRealY  = srcY  + y;
-        var destRealX = destX + x;
-        var destRealY = destY + y;
-
-        var srcIdx  = (srcRealY  * src.width  + srcRealX ) * 4;
-        var destIdx = (destRealY * dest.width + destRealX) * 4;
-
-        if (!invert) {
-          dest.data[destIdx+0] = src.data[srcIdx+0];
-          dest.data[destIdx+1] = src.data[srcIdx+1];
-          dest.data[destIdx+2] = src.data[srcIdx+2];
-        } else {
-          dest.data[destIdx+0] = 255 - src.data[srcIdx+0];
-          dest.data[destIdx+1] = 255 - src.data[srcIdx+1];
-          dest.data[destIdx+2] = 255 - src.data[srcIdx+2];
-        }
-        // copy alpha unaffected by inversion rules
-        dest.data[destIdx+3] = src.data[srcIdx+3];
-      }
-    }
-  }
-
   buildTable(width, height) {
     var dx = [ 1, 0, -1, 0 ];
     var dy = [ 0, -1, 0, 1 ];
@@ -95,7 +64,10 @@ class FLMask {
     for (x = d = i = 0, y = height - 1; i < width * height; i++) {
       xMap[i] = x;
       yMap[i] = y;
-      table[y][x] = this.FLCell(i, width * height - i - 1);
+      table[y][x] = {
+        no: i,
+        pair: width * height - i - 1
+      };
       x += dx[d];
       y += dy[d];
       if (x < 0 || width <= x || y < 0 || height <= y || (table[y][x] !== null && 0 <= table[y][x].no)) {
@@ -107,45 +79,23 @@ class FLMask {
       }
     }
 
-    return this.FLTable(table, xMap, yMap);
+    return { table, xMap, yMap };
   }
 
+  /**
+   * Given an FL Tranformation table and X and Y source position (in block coordinates), look up the intended destination block coordinates, and whether to invert the colors.
+   *
+   * @param {object} table
+   * @param {number} x
+   * @param {number} y
+   * @returns
+   * @memberof FLMask
+   */
   tableTransform(table, x, y) {
     var rx = table.xMap[table.table[y][x].pair];
     var ry = table.yMap[table.table[y][x].pair];
     var rinv = table.table[y][x].no !== table.table[y][x].pair;
-    return this.FLTransformResult(rx, ry, rinv);
-  }
-
-  FLTransformResult(x, y, inv) {
-    console.assert(typeof x === "number");
-    console.assert(typeof y === "number");
-    console.assert(typeof inv === "boolean");
-    return { x:x, y:y, inv:inv };
-  }
-
-  FLTable(table, xMap, yMap) {
-    console.assert(table instanceof Array);
-    console.assert(table.length);
-    console.assert(table[0] instanceof Array);
-    console.assert(table[0].length);
-    console.assert(table[0][0] instanceof Object); // FLCell
-
-    console.assert(xMap instanceof Array);
-    console.assert(xMap.length);
-    console.assert(typeof xMap[0] === "number");
-
-    console.assert(yMap instanceof Array);
-    console.assert(yMap.length);
-    console.assert(typeof yMap[0] === "number");
-
-    return { table:table, xMap:xMap, yMap:yMap };
-  }
-
-  FLCell(no, pair) {
-    console.assert(typeof no === "number");
-    console.assert(typeof pair === "number");
-    return { no:no, pair:pair };
+    return { x:rx, y:ry, inv:rinv };
   }
 }
 
