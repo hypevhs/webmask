29be9d13314d1ce4f1800ecb7ed1847d09b1d856
libjared <jared@perrycode.com>
2017-09-19 14:18:23 -0400

    video mask support
    needs a better props/state, as well as using requestAnimationFrame

--- src/CanvasSet.js
+++ src/CanvasSet.js
@@ -4,6 +4,35 @@ import RectangleSelector from './RectangleSelector.js';
 import './CanvasSet.css';
 
 class CanvasSet extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.props.image can be a HTMLImageElement (aka Image()), or a URL to a video.
+
+    if (this.props.image instanceof HTMLImageElement) {
+      this.state = {
+        drawable: this.props.image
+      };
+    } else if (typeof this.props.image === "string") {
+      // it's a video URL. we need to set a timer
+      // todo: requestAnimationFrame instead.
+      this.state = {
+        videoUpdateIntervalId: setInterval(() => { this.videoUpdateFrame(); }, 100),
+        drawable: new Image(100, 100) // a loading image while the video element is still rendering
+      };
+    }
+
+    this.getVideoElement = this.getVideoElement.bind(this);
+    this.videoUpdateFrame = this.videoUpdateFrame.bind(this);
+  }
+
+  videoUpdateFrame() {
+    this.setState({
+      drawable: this.videoElement
+    });
+    console.log('.');
+  }
+
   render() {
     const style = {
       width: this.getWidth(),
@@ -13,24 +42,55 @@ class CanvasSet extends Component {
     return (
       <div style={style} className="canvas-set">
         <Canvas
-            image={this.props.image}
-            width={this.getWidth()}
-            height={this.getHeight()}
-            masks={this.props.masks} />
-          <RectangleSelector
-            width={this.getWidth()}
-            height={this.getHeight()}
-            onSelection={this.props.onSelection} />
+          image={this.state.drawable}
+          width={this.getWidth()}
+          height={this.getHeight()}
+          masks={this.props.masks}
+        />
+        <RectangleSelector
+          width={this.getWidth()}
+          height={this.getHeight()}
+          onSelection={this.props.onSelection}
+        />
+        {this.getVideoElement()}
       </div>
     );
   }
 
+  getVideoElement() {
+    if (this.props.image instanceof HTMLImageElement) {
+      return null; // not a video, it's an image, so don't render any <video> element
+    }
+
+    const onLoadedData = (e) => {
+      // you might also consider onLoadedMetadata
+      this.setState({
+        videoWidth: e.target.videoWidth,
+        videoHeight: e.target.videoHeight
+      });
+    };
+
+    return <video
+      style={{ display: 'none' }}
+      src={this.props.image}
+      autoPlay
+      loop
+      ref={(me) => { this.videoElement = me; }}
+      onLoadedData={(e) => onLoadedData(e)}
+    />;
+  }
+
+  componentWillUnmount() {
+    // stop the invisible video-to-canvas drawing loop
+    clearInterval(this.state.videoUpdateIntervalId);
+  }
+
   getWidth() {
-    return this.props.image.naturalWidth;
+    return this.state.drawable.naturalWidth || this.state.videoWidth; // todo: real dimensions
   }
 
   getHeight() {
-    return this.props.image.naturalHeight;
+    return this.state.drawable.naturalHeight || this.state.videoWidth;
   }
 }
 
--- src/FullScreenDropZone.js
+++ src/FullScreenDropZone.js
@@ -5,7 +5,8 @@ class FullScreenDropZone extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dropzoneActive: false
+      dropzoneActive: false,
+      tempVideoSrc: null
     }
 
     this.onDragEnter = this.onDragEnter.bind(this);
@@ -34,12 +35,40 @@ class FullScreenDropZone extends Component {
 
   onDropAccepted(files) {
     console.log(files);
+    this.setState({ tempVideoTag: null });
+    if (files[0].type === "video/mp4") {
+      this.tryLoadVideo(files[0]);
+    } else {
+      this.tryLoadImage(files[0]);
+    }
+  }
+
+  tryLoadImage(file) {
     const img = new Image();
     img.onload = () => {
       console.log("image loaded...");
       this.props.onNewImage(img);
     }
-    img.src = URL.createObjectURL(files[0]);
+    img.src = URL.createObjectURL(file);
+  }
+
+  tryLoadVideo(file) {
+    const tempVideoSrc = URL.createObjectURL(file);
+    this.setState({ tempVideoSrc });
+  }
+
+  getTempVideo() {
+    // if we have a video file to load, let's use a temporary video tag with its events.
+    return <video
+      style={{ display: 'none' }}
+      src={this.state.tempVideoSrc}
+      onLoadedData={() => {
+        // or you could try onLoadedMetadata
+        console.log('onLoadedData');
+        this.props.onNewImage(this.state.tempVideoSrc);
+      }}
+      onPlay={() => { console.log('onPlay'); }}
+    />;
   }
 
   render() {
@@ -47,12 +76,13 @@ class FullScreenDropZone extends Component {
       <Dropzone
         disableClick
         style={{}}
-        accept='image/gif, image/jpeg, image/png, image/svg+xml'
+        accept='image/gif, image/jpeg, image/png, image/svg+xml, video/mp4'
         onDragEnter={this.onDragEnter}
         onDragLeave={this.onDragLeave}
         onDrop={this.onDrop}
         onDropAccepted={this.onDropAccepted}
       >
+        { this.state.tempVideoSrc && this.getTempVideo() }
         { this.state.dropzoneActive && <div className='dropzone-overlay'>Drop image files...</div> }
         {this.props.children}
       </Dropzone>
